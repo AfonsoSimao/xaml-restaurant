5ec951e36e02b019838ef948e1b4edd117c2fc10
AfonsoSimao <afonso.simao@computedlife.com>
2015-10-12 14:16:09 +0100

    Layout and navigation
    Added the views that will receive the data;
    Implemented the buttons to navigate through the pages.

--- RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
+++ RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
@@ -1,4 +1,17 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -13,6 +26,10 @@ namespace RestaurantManager.UniversalWindows
         {
             this.InitializeComponent();
         }
-        
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
     }
 }
